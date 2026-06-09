3eb010ea1e18f21ac295911b67e17911c980fa17
camilakolakovich68-cell <camilakolakovich68@gmail.com>
2026-06-08 21:19:38 -0300

    Delete SGE-asistencia-main directory

--- SGE-asistencia-main/asistencia1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import RegistroAsistencia
-
-
-@admin.register(RegistroAsistencia)
-class RegistroAsistenciaAdmin(admin.ModelAdmin):
-    list_display = ('alumno', 'materia', 'fecha', 'estado')
-    list_filter = ('fecha', 'estado', 'materia')
-    search_fields = ('alumno__nombre',)
